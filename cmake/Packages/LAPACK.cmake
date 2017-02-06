d564564d575b792d920e85ae2530f69410795c5d
Jakob van Santen <jvansanten@gmail.com>
2017-02-06 07:52:10 -0600

    Teach LAPACK.cmake to find OpenBLAS

--- cmake/Packages/LAPACK.cmake
+++ cmake/Packages/LAPACK.cmake
@@ -12,7 +12,22 @@
 #   LAPACK_LDFLAGS                                                             #
 ################################################################################
 
-LIST (APPEND _blas_vendors "Goto" "ATLAS" "Apple" "Generic")
+LIST (APPEND _blas_vendors "OpenBLAS" "Goto" "ATLAS" "Apple" "Generic")
+
+# old CMake doesn't know that OpenBLAS is a BLAS
+IF (${CMAKE_VERSION} VERSION_LESS 3.6)
+  IF (NOT LAPACK_FOUND)
+    set(_vendor OpenBLAS)
+    FIND_PACKAGE(${_vendor} QUIET)
+    IF (OpenBLAS_FOUND)
+      SET (LAPACK_LIBRARIES "${${_vendor}_LIBRARIES}")
+      MESSAGE (STATUS "LAPACK found (${_vendor})")
+      MESSAGE (STATUS "  * libs:     ${${_vendor}_LIBRARIES}")
+      SET (LAPACK_FOUND TRUE)
+      SET (BLAS_FOUND TRUE)
+    ENDIF ()
+  ENDIF ()
+ENDIF ()
 
 IF (NOT LAPACK_FOUND)
 
@@ -20,7 +35,6 @@ IF (NOT LAPACK_FOUND)
     SET (BLA_VENDOR ${_vendor})
     FIND_PACKAGE (LAPACK QUIET)
     IF (LAPACK_FOUND)
-      SET (LAPACK_LDFLAGS "${LAPACK_LIBRARIES}")
       MESSAGE (STATUS "LAPACK found (${_vendor})")
       MESSAGE (STATUS "  * libs:     ${LAPACK_LIBRARIES}")
       BREAK ()
