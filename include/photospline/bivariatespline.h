8b6eee3726ee50e354e3315d5ee5cef8b7e21ffc
Matti Jansson <matti.jansson@fysik.su.se>
2022-10-27 14:31:36 +0200

    Bug fixes.

--- include/photospline/bivariatespline.h
+++ include/photospline/bivariatespline.h
@@ -8,38 +8,45 @@ namespace photospline{
 	enum splinetype{
 		bspline, 
 		bivariatespline
-	}
+	};
 
 	class bspline
 	{
 	public:
 		double eval(int const splineid, double const x) const;	
-	}
+	};
 	class bivariatespline
 	{
 	private:
-
 		class vertex
 		{
 		public:
 			double x,y;
-			vertex(x,y):x(x),y(y){}
-		}
+			vertex(float x,float y):x(x),y(y){}
+		};
 		class triangle
 		{
+		public:
 			vertex v1, v2, v3;
 			std::vector<vertex> neighbours;
-			Vector3f dim1;
-			Vector3f dim2;
-			Vector3f normal;
-			Vector3f point;
-		}
+			Eigen::Vector3f dim1;
+			Eigen::Vector3f dim2;
+			Eigen::Vector3f normal;
+			Eigen::Vector3f point;
+		};
 		std::vector<triangle> triangles;
 
-		double det(vertex v1,vertex v2,vertex v3);
+		static double det(vertex v1,vertex v2,vertex v3);
+		static std::tuple<vertex, vertex,vertex> makerighthanded(vertex v1,vertex v2,vertex v3);
+		static vertex projecttoplane(Eigen::Vector3f x, triangle t);
+		static std::tuple<double, double, double> barycentriccoordinates(vertex x, vertex v1, vertex v2, vertex v3);
+		static float chi(vertex x, vertex v1, vertex v2, vertex v3);
+		static double M1(vertex x, vertex v1, vertex v2, vertex v3);
+		static double M2(vertex x, vertex v1, vertex v2, vertex n1, vertex n2);
+		static double M3(vertex x, vertex v1, vertex v2, vertex v3, vertex n1, vertex n2);
 	public:
 		double eval(int const splineid, double const theta, double const phi) const;	
-	}
+	};
 } //namespace photospline
 
 #endif /* PHOTOSPLINE_CORE_BIVARIATESPLINE_H */
--- src/core/bivariatespline.cpp
+++ src/core/bivariatespline.cpp
@@ -1,98 +1,108 @@
 #include "photospline/bivariatespline.h"
 #include <cmath>
 
-double bivariatespline::det(vertex v1,vertex v2,vertex v3)
+namespace photospline
 {
-	return v2.x*v3.y - v3.x*v2.y - v1.x*v3.y + v3.x*v1.y + v1.x*v3.y - v3.y*v1.x;
-}
-
-std::tuple<int, int, int> bivariatespline::makerighthanded(vertex v1,vertex v2,vertex v3)
-{
-	if det(v1, v2, v3) < 0
+	double bivariatespline::det(vertex v1,vertex v2,vertex v3)
 	{
-		return std::tuple(v1, v3, v2);
+		return v2.x*v3.y - v3.x*v2.y - v1.x*v3.y + v3.x*v1.y + v1.x*v3.y - v3.y*v1.x;
 	}
-	else
+
+	std::tuple<bivariatespline::vertex, bivariatespline::vertex, bivariatespline::vertex> bivariatespline::makerighthanded(vertex v1,vertex v2,vertex v3)
 	{
-		return std::tuple(v1, v2, v3);
+		if (det(v1, v2, v3) < 0)
+		{
+			return std::tuple<bivariatespline::vertex, bivariatespline::vertex, bivariatespline::vertex>(v1, v3, v2);
+		}
+		else
+		{
+			return std::tuple<bivariatespline::vertex, bivariatespline::vertex, bivariatespline::vertex>(v1, v2, v3);
+		}
 	}
-}
 
-vertex projecttoplane(Vector3f x, triangle t)
-{
-	double prod1 = t.point.dot(t.normal);
-	double prod2 = x.dot(t.normal);
-	double prod3 = prod1 / prod2;
-	Vector3f planeintersection = x * prod3;
-	return vertex(t.dim1.dot(planeintersection), t.dim2.dot(planeintersection))
-}
-
-std::tuple<double, double, double> barycentriccoordinates(vertex x, vertex v1, vertex v2, vertex v3);
-{
-	double denominator = det(v1, v2, v3);
-	return std::tuple(det(x, v2, v3)/denominator, det(v1, x, v3)/denominator, det(v1, v2, x)/denominator);
-}
-
-int chi(vertex x, vertex v1, vertex v2, vertex v3)
-{
-	auto x = barycentriccoordinates(x, v1, v2, v3);
-	if (x[0] < 0 || x[1] < 0 || x[2] < 0)
+	bivariatespline::vertex bivariatespline::projecttoplane(Eigen::Vector3f x, triangle t)
 	{
-		return 0;
+		double prod1 = t.point.dot(t.normal);
+		double prod2 = x.dot(t.normal);
+		double prod3 = prod1 / prod2;
+		Eigen::Vector3f planeintersection = x * prod3;
+		return vertex(t.dim1.dot(planeintersection), t.dim2.dot(planeintersection));
 	}
-	else
+
+	std::tuple<double, double, double> bivariatespline::barycentriccoordinates(bivariatespline::vertex x, bivariatespline::vertex v1, bivariatespline::vertex v2, bivariatespline::vertex v3)
 	{
-		return 1;
+		double denominator = det(v1, v2, v3);
+		return std::tuple<double, double, double>(det(x, v2, v3)/denominator, det(v1, x, v3)/denominator, det(v1, v2, x)/denominator);
 	}
-}
 
-double M1(vertex x, vertex v1, vertex v2, vertex v3)
-{
-	if det(v1, v2, v3) < 0
+	float bivariatespline::chi(bivariatespline::vertex x, bivariatespline::vertex v1, bivariatespline::vertex v2, bivariatespline::vertex v3)
 	{
-		return chi(x, v1, v2, v3)/det(v1, v3, v2);
+		double x1, x2, x3;
+		std::tie(x1, x2, x3) = barycentriccoordinates(x, v1, v2, v3);
+		if (x1 < 0 || x2 < 0 || x3 < 0)
+		{
+			return 0;
+		}
+		else if (x1 == 0 || x2 == 0 || x3 == 0)
+		{
+			return 0.5;
+		}
+		else
+		{
+			return 1;
+		}
 	}
-	else
+
+	double bivariatespline::M1(bivariatespline::vertex x, bivariatespline::vertex v1, bivariatespline::vertex v2, bivariatespline::vertex v3)
 	{
-		return chi(x, v1, v2, v3)/det(v1, v2, v3);	
+		if (det(v1, v2, v3) < 0)
+		{
+			return chi(x, v1, v2, v3)/det(v1, v3, v2);
+		}
+		else
+		{
+			return chi(x, v1, v2, v3)/det(v1, v2, v3);	
+		}
 	}
-}
 
-double M2(vertex x, vertex v1, vertex v2, vertex n1, vertex n2)
-{
-	double result = 0;
-	std::tuple<double, double, double> bcoords = barycentriccoordinates(x, v2, n1, n2);
-	result += bcoords[0]*M1(x, v1, n1, n2);
-	result += bcoords[1]*M1(x, v1, v2, n2);
-	result += bcoords[2]*M1(x, v1, v2, n1);
-	return result;
-}
-
-double M3(vertex x, vertex v1, vertex v2, vertex v3, vertex n1, vertex n2)
-{
-	double result = 0;
-	std::tuple<double, double, double> bcoords = barycentriccoordinates(x, v1, v2, v3);
-	result += bcoords[0]*M2(x, v2, v3, n1, n2);
-	result += bcoords[1]*M2(x, v1, v3, n1, n2);
-	result += bcoords[2]*M2(x, v1, v2, n1, n2);
-	return result;
-}
-
-
-double bivariatespline::eval(int const splineid, double const theta, double const phi)
-{
-	Vector3f tempx(std::sin(theta)*std::cos(phi), std::sin(theta)*std::sin(phi), std::cos(theta));
-	triangle t = trianles[splineid];
-	vertex x = projecttoplane(tempx, t.normal, t.planepoint, t)
-	double result = 0;
-	int size = neighbours.size();
-	for(i=0; i++; i<size)
+	double bivariatespline::M2(bivariatespline::vertex x, bivariatespline::vertex v1, bivariatespline::vertex v2, bivariatespline::vertex n1, bivariatespline::vertex n2)
 	{
-		n1 = neighbours[i];
-		n2 =  neighbours[(i+1)%size]
-		result += det(n1, n2, x)* M3(x, t.v1, t.v2, t.v3, n1, n2);
+		double result = 0;
+		double x1, x2, x3;
+		std::tie(x1, x2, x3) = barycentriccoordinates(x, v2, n1, n2);
+		result += x1*M1(x, v1, n1, n2);
+		result += x2*M1(x, v1, v2, n2);
+		result += x3*M1(x, v1, v2, n1);
+		return result;
 	}
-	return result;
+
+	double bivariatespline::M3(bivariatespline::vertex x, bivariatespline::vertex v1, bivariatespline::vertex v2, bivariatespline::vertex v3, bivariatespline::vertex n1, bivariatespline::vertex n2)
+	{
+		double result = 0;
+		double x1, x2, x3;
+		std::tie(x1, x2, x3) = barycentriccoordinates(x, v1, v2, v3);
+		result += x1*M2(x, v2, v3, n1, n2);
+		result += x2*M2(x, v1, v3, n1, n2);
+		result += x3*M2(x, v1, v2, n1, n2);
+		return result;
+	}
+
+
+	double bivariatespline::eval(int const splineid, double const theta, double const phi) const
+	{
+		Eigen::Vector3f tempx(std::sin(theta)*std::cos(phi), std::sin(theta)*std::sin(phi), std::cos(theta));
+		triangle t = triangles[splineid];
+		vertex x = projecttoplane(tempx, t);
+		double result = 0;
+		int size = t.neighbours.size();
+		for(int i=0; i<size; i++)
+		{
+			auto n1 = t.neighbours[i];
+			auto n2 =  t.neighbours[(i+1)%size];
+			result += det(n1, n2, x)* M3(x, t.v1, t.v2, t.v3, n1, n2);
+		}
+		return result;
+
+	}
 
 }
-
