15bfc2e4602cc29a5ee6684e6bc0a2a858cf6384
Jakob van Santen <jvansanten@gmail.com>
2023-10-19 18:28:54 +0200

    Add a test for evaluate
    and adjust annotations to cover the vector case

--- test/test_eval.py
+++ test/test_eval.py
@@ -7,25 +7,30 @@ import numpy as np
 
 from pathlib import Path
 
-class TestEvaluation(unittest.TestCase):
 
+class TestEvaluation(unittest.TestCase):
     def setUp(self):
         self.testdata = Path(__file__).parent / "test_data"
         self.spline = photospline.SplineTable(self.testdata / "test_spline_4d.fits")
         extents = np.array(self.spline.extents)
-        loc = extents[:,:1]
+        loc = extents[:, :1]
         scale = np.diff(extents, axis=1)
         self.x = (np.random.uniform(0, 1, size=(self.spline.ndim, 10)) + loc) * scale
 
-    def test_searchcenters_vector(self):
+    def test_vector(self):
         centers = self.spline.search_centers(self.x)
         self.assertIsInstance(centers, np.ndarray)
         self.assertEqual(centers.shape, (self.spline.ndim, self.x.shape[1]))
-    
-    def test_searchcenters_scalar(self):
+        v = self.spline.evaluate(self.x, centers=centers)
+        self.assertIsInstance(v, np.ndarray)
+        self.assertEqual(v.shape, (self.x.shape[1],))
+
+    def test_scalar(self):
         centers = self.spline.search_centers([x[0] for x in self.x])
         self.assertIsInstance(centers, tuple)
         self.assertEqual(len(centers), self.spline.ndim)
+        v = self.spline.evaluate([x[0] for x in self.x], centers=centers)
+        self.assertIsInstance(v, float)
 
 
 if __name__ == "__main__":
